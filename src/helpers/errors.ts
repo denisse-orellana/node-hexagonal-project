55cae3ecc1b6828deccb053dab148a2507bdc1de
Denisse Orellana <denissep.orellana@gmail.com>
2023-06-20 18:21:02 -0400

    add: bootstrap base and server initialization

--- src/helpers/errors.ts
+++ src/helpers/errors.ts
@@ -2,6 +2,6 @@ import { Request, Response } from 'express'
 
 export default class {
   static notFound(_req: Request, res: Response): void {
-    res.status(404).send('Not found')
+    res.status(404).send('Route Not found')
   }
 }

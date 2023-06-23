3f78c9089c5d047d4ca1fd7e9842e0862a59be0a
Denisse Orellana <denissep.orellana@gmail.com>
2023-06-23 12:34:53 -0400

    change: respository

--- src/modules/user/domain/patientUser.repository.ts
+++ src/modules/user/domain/patientUser.repository.ts
@@ -5,9 +5,9 @@ import { PatientUserNotFoundException } from './exceptions/patientUser.exception
 // SOLID Principle: Inversion Dependency
 export interface PatientUserRepository {
   // Design Pattern: Facade
-  list(): Promise<PatientUser[]>
-  listOne(guid: string): Promise<PatientUser>
   insert(patientUser: PatientUser): Promise<PatientUser>
+  list(): Promise<PatientUser[]>
+  listOne(guid: string): Promise<Result<PatientUser, PatientUserNotFoundException>>
   update(guid: string, patientUser: Partial<PatientUserUpdate>): Promise<Result<PatientUser, PatientUserNotFoundException>>
-  delete(guid: string): Promise<PatientUser>
+  delete(guid: string): Promise<Result<PatientUser, PatientUserNotFoundException>>
 }

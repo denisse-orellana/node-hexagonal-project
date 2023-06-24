8bc648db70b805729236afd120450a0c3f76ee4a
Denisse Orellana <denissep.orellana@gmail.com>
2023-06-23 20:45:16 -0400

    add: insert and list one to dto

--- src/modules/user/interfaces/http/dto/patientUser-list-one.dto.ts
+++ src/modules/user/interfaces/http/dto/patientUser-list-one.dto.ts
@@ -1,23 +1,21 @@
 import { PatientUserProperties } from 'src/modules/user/domain/patientUser'
 import { DTO } from './dto.generic'
 
-interface PatientUserDTO {
+interface PatientUserOneDTO {
   name: string
   lastname: string
   email: string
-  cellphone: string
   guid: string
 }
 
-export type PatientUserInsertOneDTO = PatientUserDTO
+export type PatientUserListOneDTO = PatientUserOneDTO
 
-export class PatientUserInsertMapping extends DTO<PatientUserProperties, PatientUserInsertOneDTO> {
-  execute(data: PatientUserProperties): PatientUserInsertOneDTO {
+export class PatientUserListOneMapping extends DTO<PatientUserProperties, PatientUserListOneDTO> {
+  execute(data: PatientUserProperties): PatientUserListOneDTO {
     return {
       name: data.name,
       lastname: data.lastname,
       email: data.email.value,
-      cellphone: data.cellphone,
       guid: data.guid,
     }
   }

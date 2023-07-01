2986769f13b3ec051182d63be128c3ed7c9b8519
Denisse Orellana <denissep.orellana@gmail.com>
2023-06-30 21:51:28 -0400

    add: min length to update and delete validator

--- src/modules/user/interfaces/http/validators/patientUserDelete.validator.ts
+++ src/modules/user/interfaces/http/validators/patientUserDelete.validator.ts
@@ -1,8 +1,9 @@
-import { IsNotEmpty, IsString } from 'class-validator'
+import { IsNotEmpty, IsString, MinLength } from 'class-validator'
 
 export class PatientUserDeleteValidator {
   // Design Pattern: Decorator
   @IsString({ message: 'Guid must be a string' })
   @IsNotEmpty({ message: 'Guid must not be empty' })
+  @MinLength(10, { message: 'Guid is too short' })
   guid: string
 }

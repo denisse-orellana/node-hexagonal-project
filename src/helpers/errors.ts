c5239efa44a8fbd28e8e636d79249cc4c45ece93
Denisse Orellana <denissep.orellana@gmail.com>
2023-07-02 19:10:00 -0400

    add: generic error errors

--- src/helpers/errors.ts
+++ src/helpers/errors.ts
@@ -1,7 +1,20 @@
+// import { Request, Response } from 'express'
+
+// export default class {
+//   static notFound(_req: Request, res: Response): void {
+//     res.status(404).send('Route Not found')
+//   }
+// }
+
 import { Request, Response } from 'express'
+import { IError } from 'src/modules/user/interfaces/helpers/ierror'
 
 export default class {
   static notFound(_req: Request, res: Response): void {
     res.status(404).send('Route Not found')
+  }
+
+  static genericError(error: IError, req: Request, res: Response): void {
+    res.status(error.status || 500)
   }
 }
